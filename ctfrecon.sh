3f59d9a8bb08e944e20d117b068a962f2c1b51da
akash45ace <akash45ace@gmail.com>
2023-02-02 14:40:49 +0530

    simplified

--- ctfrecon.sh
+++ ctfrecon.sh
@@ -1,31 +1,26 @@
 #!/bin/bash
-echo -e "\e[31m" '[+] 1. print your ip infrmation.
- [+] 2. do a full ctf recon[start script with root]'
-echo -e '\e[0m'
+echo -e '\n \033[1m ### Written by Akash ###\033[0m\n'
+echo -e "\033[31m[+] 1. Print your ip infrmation.
+[+] 2. Full ctf recon[start script with root].\033[0m"
 read num
 if [ $num -eq 1 ]; then
 	ifconfig
 elif [ $num -eq 2 ]; then
-	echo ' Enter the ip for port scan'
+	echo ' Enter the ip for port scan:'
 	read ip
-	echo -e "\e[32m"	
-	echo '[*]  port scan'
-	echo -e "\e[0m"
+	echo -e "\033[32m[*]  port scanning...\033[0m"
 	nmap -sS $ip | grep -A 30 'Nmap scan report'
-	echo -e "\e[32m"
-	echo '[*] port enumeration'
-	echo -e "\e[0m"
+	echo -e "\033[32m[*] port enumerating...\033[0m"
 	nmap -sV -sC $ip | grep -A 30 "Nmap scan report"
-	echo -e "\e[32m"
-	echo '[*]directory enum at 80'
-	echo -e "\e[0m"
-	gobuster dir -u http://$ip:80 -w /usr/share/SecLists-master/Discovery/Web-Content/common.txt | grep -A 50 'Starting gobuster'
-	echo 'if it is on another port give me port no'
+	echo -e "\033[32m[*]directory enumerating... at 80\033[0m"
+	gobuster dir -u http://$ip:80 -w /usr/share/wordlists/dirb/common.txt | grep -A 50 'Starting gobuster'
+	echo 'for dir-enum on another port give the port no'
+	echo 'or press enter to exit.'
 	read port
 	if [ $# -eq 0 ]; then
 	       echo 'no imput given'
 	else
- 		gobuster dir -u http://$ip:$port -w /usr/share/SecLists-master/Discovery/Web-Content/common.txt | grep -A 50 'Starting gobuster'
+ 		gobuster dir -u http://$ip:$port -w /usr/share/wordlists/dirb/common.txt | grep -A 50 'Starting gobuster'
 	fi
 	
 	
